fdb378d662899b9a5b0f0185d504e43c3deb18f0
Liam Sweeney <liamsweeney569@gmail.com>
2019-01-22 16:39:10 -0400

    Setting up Routes. Sec 6, Lec 54. Updated Compatibility, set up routes

--- client/main.js
+++ client/main.js
@@ -1,11 +1,33 @@
+/* 
+ * So I ended up having to follow a Q/A from sec 6 lec 54 which 
+ * described how to set up routing with the newer version of react-router.
+ * It also works if you go back and use the specific versions that he uses,
+ * which I may end up doing if newer versions complicate things further
+ */
+
 import {Meteor} from 'meteor/meteor';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 
 import './main.html'; //I have no idea why it needs this when the last project didnt
 import Signup from '../imports/ui/Signup';
 import Link from '../imports/ui/Link';
+import NotFound from '../imports/ui/NotFound';
+import Login from '../imports/ui/Login';
+
+const routes = (
+  <Router>
+    <Switch>
+      <Route exact path ="/" component={Login}/>
+      <Route exact path ="/signup" component={Signup}/>
+      <Route exact path ="/links" component={Link}/>
+      <Route exact path ="*" component={NotFound}/>
+    </Switch>
+  </Router>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<Link/>, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 });
