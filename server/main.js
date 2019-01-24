13ddf8a03d44a0cbd5ab25304650ad57d18bcba4
Liam Sweeney <liam.sweeney@amirix.com>
2019-01-24 16:41:49 -0400

    Schema Validation, Sec 6, Lec 62

--- server/main.js
+++ server/main.js
@@ -1,5 +1,24 @@
 import { Meteor } from 'meteor/meteor';
+import SimpleSchema from 'simpl-schema'; 
+//Had to update to simpl-schema@1.5.0
+//His version was causing weird errors
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  // const employeeSchema = new SimpleSchema({
+  //   name: {
+  //     type: String,
+  //     min: 1,
+  //     max: 200,
+  //   },
+  //   hourlyWage: {
+  //     type: Number,
+  //     min: 0
+  //   },
+  //   email: {
+  //     type: String,
+  //     regEx: SimpleSchema.RegEx.Email
+  //   }
+  // });
 });
