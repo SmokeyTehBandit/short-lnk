8d45ab3183996138be471d5554f5a0dcf2ce2de5
Liam Sweeney <liam.sweeney@amirix.com>
2019-01-25 15:44:34 -0400

    New User Validation. Sec 6, Lec 63

--- server/main.js
+++ server/main.js
@@ -1,10 +1,28 @@
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema from 'simpl-schema'; 
+import {Accounts} from 'meteor/accounts-base';
 //Had to update to simpl-schema@1.5.0
 //His version was causing weird errors
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  Accounts.validateNewUser((user) => {
+    const email = user.emails[0].address;
+
+    try{
+      new SimpleSchema({
+        email: {
+          type: String,
+          regEx: SimpleSchema.RegEx.Email
+        }
+      }).validate({email});
+    }catch(e){
+      throw new Meteor.Error(400, e.message);
+    }
+
+    return true;
+  });
 
   // const employeeSchema = new SimpleSchema({
   //   name: {
